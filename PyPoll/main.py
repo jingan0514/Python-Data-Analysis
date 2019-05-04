92478a5bfc7b3e4c6ae75a05c30ef2a03d8e6243
GanJin <gj2297@columbia.edu>
2019-05-04 08:07:07 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,7 +16,7 @@ with open(csvpath, newline = '') as csvfile:
         candidate.update({row[2]: 1})
         candidate_list = list(candidate.keys())
 
-textpath = os.path.join('.', 'Election Resultss.txt')
+textpath = os.path.join('.', 'Election Results.txt')
 with open(textpath, 'w') as text:
 
     print('Election Results')
